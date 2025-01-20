0592dee2caeaa234052974139b89bc21612fcdc3
Jay <jaykhavadia@gmail.com>
2025-01-20 21:08:11 +0530

    removed build

--- dist/src/cronservice.d.ts
+++
@@ -1,6 +0,0 @@
-import { AuthTokenService } from './common/auth-token/auth-token.service';
-export declare class CronService {
-    private readonly authTokenService;
-    constructor(authTokenService: AuthTokenService);
-    refreshAccessToken(): Promise<void>;
-}
