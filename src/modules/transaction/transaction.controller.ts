7c5918cf23370780b941870d5a33ba551c709c0e
Jay <jaykhavadia@gmail.com>
2025-01-20 21:32:42 +0530

    - added validation

--- src/modules/transaction/transaction.controller.ts
+++ src/modules/transaction/transaction.controller.ts
@@ -1,23 +1,41 @@
+import { Messages } from "./../../constants/messages.constants";
 import {
-  Controller, Post, Put, Get, Delete, Param, Body, UploadedFiles, UseInterceptors, ValidationPipe, HttpException, HttpStatus, Logger,
+  Controller,
+  Post,
+  Put,
+  Get,
+  Delete,
+  Param,
+  Body,
+  UploadedFiles,
+  UseInterceptors,
+  ValidationPipe,
+  HttpException,
+  HttpStatus,
+  Logger,
   UseGuards,
   Query,
   UploadedFile,
   Patch,
-} from '@nestjs/common';
-import { TransactionService } from './transaction.service';
-import { FileFieldsInterceptor, FileInterceptor } from '@nestjs/platform-express';
-import { Transaction } from './entities/transaction.entity';
-import { FilesHelperService } from 'src/common/files-helper/files-helper.service';
-import { Messages } from 'src/constants/messages.constants';
-import { CreateTransactionDto, UpdateTransactionDto } from './dto/CreateTransaction.dto';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { UploadService } from 'src/common/upload/upload.service';
-import { Sheet } from '../sheet/entities/sheet.entity';
-import { format } from 'date-fns';
-import { SheetService } from '../sheet/sheet.service';
+} from "@nestjs/common";
+import { TransactionService } from "./transaction.service";
+import {
+  FileFieldsInterceptor,
+  FileInterceptor,
+} from "@nestjs/platform-express";
+import { Transaction } from "./entities/transaction.entity";
+import { FilesHelperService } from "src/common/files-helper/files-helper.service";
+import {
+  CreateTransactionDto,
+  UpdateTransactionDto,
+} from "./dto/CreateTransaction.dto";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { UploadService } from "src/common/upload/upload.service";
+import { Sheet } from "../sheet/entities/sheet.entity";
+import { format } from "date-fns";
+import { SheetService } from "../sheet/sheet.service";
 
-@Controller('transaction')
+@Controller("transaction")
 @UseGuards(JwtAuthGuard)
 export class TransactionController {
   private readonly logger = new Logger(TransactionController.name);
@@ -27,7 +45,7 @@ export class TransactionController {
     private readonly uploadService: UploadService,
     private readonly sheetService: SheetService,
     private readonly filesHelperService: FilesHelperService,
-  ) { }
+  ) {}
 
   /**
    * Creates a new transaction.
@@ -36,52 +54,77 @@ export class TransactionController {
    * @returns The created transaction.
    */
   @Post()
-  @UseInterceptors(FileFieldsInterceptor([
-    { name: 'vehiclePictures[back]', maxCount: 1 },
-    { name: 'vehiclePictures[front]', maxCount: 1 },
-    { name: 'vehiclePictures[left]', maxCount: 1 },
-    { name: 'vehiclePictures[right]', maxCount: 1 },
-  ]))
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: "vehiclePictures[back]", maxCount: 1 },
+      { name: "vehiclePictures[front]", maxCount: 1 },
+      { name: "vehiclePictures[left]", maxCount: 1 },
+      { name: "vehiclePictures[right]", maxCount: 1 },
+    ]),
+  )
   async create(
     @Body(new ValidationPipe()) body: CreateTransactionDto,
-    @UploadedFiles() files?: { [key: string]: Express.Multer.File[] }
+    @UploadedFiles() files?: { [key: string]: Express.Multer.File[] },
   ): Promise<response<Transaction>> {
     try {
       // const transactionData = JSON.parse(body);
       // Create the transaction first without saving the pictures yet
       let transaction = await this.transactionService.create(body);
-      if (files && Object.keys(files).length > 0) {
-        // Save the files using the generated transactionId
-        const savedFiles = await this.filesHelperService.saveTransactionFiles(files, transaction.id);
-        // Update the transaction with the saved file URLs
-        transaction = await this.transactionService.updateTransaction(transaction.id, { ...transaction, pictures: savedFiles });
+      try {
+        if (files && Object.keys(files).length > 0) {
+          // Save the files using the generated transactionId
+          const savedFiles = await this.filesHelperService.saveTransactionFiles(
+            files,
+            transaction.id,
+          );
+          // Update the transaction with the saved file URLs
+          transaction = await this.transactionService.updateTransaction(
+            transaction.id,
+            { ...transaction, pictures: savedFiles },
+          );
+        }
+        this.logger.log(Messages.transaction.createSuccess); // Log success message
+      } catch (error) {
+        console.log("TransactionController ~ error:", error);
+        throw new HttpException("Image Upload Issue", HttpStatus.BAD_REQUEST);
       }
-      this.logger.log(Messages.transaction.createSuccess); // Log success message
       return {
         success: true,
         message: Messages.transaction.createSuccess,
         data: transaction, // Return the updated transaction
       };
     } catch (error) {
-      this.logger.error(`[TransactionController] [create] Error: ${error.message}`); // Log error
-      throw new HttpException(error.message, HttpStatus.BAD_REQUEST); // Handle error
+      this.logger.error(
+        `[TransactionController] [create] Error: ${error.message}`,
+      ); // Log error
+      throw new HttpException(
+        error.message === "Image Upload Issue"
+          ? "Transaction is been Created but Image Upload Access Revoked"
+          : error.message,
+        HttpStatus.BAD_REQUEST,
+      ); // Handle error
     }
   }
 
-  @Patch(':id')
+  @Patch(":id")
   async update(
-    @Param('id') id: string,
+    @Param("id") id: string,
     @Body(new ValidationPipe()) body: UpdateTransactionDto,
   ): Promise<response<Transaction>> {
     try {
-      const updatedTransaction = await this.transactionService.update(Number(id), body);
+      const updatedTransaction = await this.transactionService.update(
+        Number(id),
+        body,
+      );
       return {
         success: true,
         message: Messages.transaction.updateSuccess(Number(id)),
         data: updatedTransaction, // Return the updated transaction
       };
     } catch (error) {
-      this.logger.error(`[TransactionController] [update] Error: ${error.message}`); // Log error
+      this.logger.error(
+        `[TransactionController] [update] Error: ${error.message}`,
+      ); // Log error
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST); // Handle error
     }
   }
@@ -101,23 +144,33 @@ export class TransactionController {
         data: transactions, // Return all transactions
       };
     } catch (error) {
-      this.logger.error(`[TransactionController] [findAll] Error: ${error.message}`); // Log error
-      throw new HttpException(Messages.transaction.findAllFailure, HttpStatus.BAD_REQUEST); // Handle error
+      this.logger.error(
+        `[TransactionController] [findAll] Error: ${error.message}`,
+      ); // Log error
+      throw new HttpException(
+        Messages.transaction.findAllFailure,
+        HttpStatus.BAD_REQUEST,
+      ); // Handle error
     }
   }
 
-  @Get('past-transaction/:vehicleNo')
-  async findPastTransaction(@Param('vehicleNo') vehicleNo: string): Promise<response<Transaction>> {
+  @Get("past-transaction/:vehicleNo")
+  async findPastTransaction(
+    @Param("vehicleNo") vehicleNo: string,
+  ): Promise<response<Transaction>> {
     const vehicleNumber = parseInt(vehicleNo, 10); // Parse the ID from the URL
     try {
-      const transaction = await this.transactionService.findPastTransaction(vehicleNumber); // Fetch all transactions
+      const transaction =
+        await this.transactionService.findPastTransaction(vehicleNumber); // Fetch all transactions
       return {
         success: true,
         message: Messages.transaction.findPastTransactionSuccess(vehicleNumber),
         data: transaction, // Return all transactions
       };
     } catch (error) {
-      this.logger.error(`[TransactionController] [findPastTransaction] Error: ${error.message}`); // Log error
+      this.logger.error(
+        `[TransactionController] [findPastTransaction] Error: ${error.message}`,
+      ); // Log error
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST); // Handle error
     }
   }
@@ -126,8 +179,8 @@ export class TransactionController {
    * Deletes a transaction by its ID.
    * @param id - The ID of the transaction to delete.
    */
-  @Delete(':id')
-  async remove(@Param('id') id: string): Promise<response<void>> {
+  @Delete(":id")
+  async remove(@Param("id") id: string): Promise<response<void>> {
     const transactionId = parseInt(id, 10); // Parse the ID from the URL
     try {
       await this.transactionService.remove(transactionId); // Remove the transaction
@@ -137,36 +190,44 @@ export class TransactionController {
         message: Messages.transaction.removeSuccess(transactionId), // Return success message
       };
     } catch (error) {
-      this.logger.error(`[TransactionController] [remove] Error: ${error.message}`); // Log error
-      throw new HttpException(Messages.transaction.removeFailure(transactionId), HttpStatus.BAD_REQUEST); // Handle error
+      this.logger.error(
+        `[TransactionController] [remove] Error: ${error.message}`,
+      ); // Log error
+      throw new HttpException(
+        Messages.transaction.removeFailure(transactionId),
+        HttpStatus.BAD_REQUEST,
+      ); // Handle error
     }
   }
 
-  @Get('filter')
+  @Get("filter")
   async getTransactionsByDate(
-    @Query('from') from?: string,
-    @Query('to') to?: string,
-    @Query('months') months?: number,
-    @Query('date') date?: string
+    @Query("from") from?: string,
+    @Query("to") to?: string,
+    @Query("months") months?: number,
+    @Query("date") date?: string,
   ): Promise<response<Transaction[]>> {
     try {
-      const transactions = await this.transactionService.getTransactionsByDateRange(
-        from,
-        to,
-        months,
-        date
-      );
+      const transactions =
+        await this.transactionService.getTransactionsByDateRange(
+          from,
+          to,
+          months,
+          date,
+        );
 
       return {
         success: true,
-        message: 'Transactions fetched successfully.',
+        message: "Transactions fetched successfully.",
         data: transactions,
       };
     } catch (error) {
-      this.logger.error(`[TransactionController] [getTransactionsByDate] Error: ${error.message}`);
+      this.logger.error(
+        `[TransactionController] [getTransactionsByDate] Error: ${error.message}`,
+      );
       throw new HttpException(
-        'Failed to fetch transactions.',
-        HttpStatus.BAD_REQUEST
+        "Failed to fetch transactions.",
+        HttpStatus.BAD_REQUEST,
       );
     }
   }
@@ -176,8 +237,8 @@ export class TransactionController {
    * @param id - The ID of the transaction to retrieve.
    * @returns The found transaction.
    */
-  @Get(':id')
-  async findOne(@Param('id') id: string): Promise<response<Transaction>> {
+  @Get(":id")
+  async findOne(@Param("id") id: string): Promise<response<Transaction>> {
     const transactionId = parseInt(id, 10); // Parse the ID from the URL
     try {
       const transaction = await this.transactionService.findOne(transactionId); // Fetch transaction by ID
@@ -188,28 +249,37 @@ export class TransactionController {
         data: transaction, // Return the found transaction
       };
     } catch (error) {
-      this.logger.error(`[TransactionController] [findOne] Error: ${error.message}`); // Log error
-      throw new HttpException(Messages.transaction.findOneFailure(transactionId), HttpStatus.BAD_REQUEST); // Handle error
+      this.logger.error(
+        `[TransactionController] [findOne] Error: ${error.message}`,
+      ); // Log error
+      throw new HttpException(
+        Messages.transaction.findOneFailure(transactionId),
+        HttpStatus.BAD_REQUEST,
+      ); // Handle error
     }
   }
 
   /**
-     * Endpoint to upload transaction from an Excel file.
-     * @param file - The Excel file containing employee data.
-     */
-  @Post('upload')
-  @UseInterceptors(FileInterceptor('file'))
-  async uploadExcel(@UploadedFile() file: Express.Multer.File): Promise<response<any>> {
+   * Endpoint to upload transaction from an Excel file.
+   * @param file - The Excel file containing employee data.
+   */
+  @Post("upload")
+  @UseInterceptors(FileInterceptor("file"))
+  async uploadExcel(
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<response<any>> {
     try {
-       const errorArray = await this.transactionService.processTransaction(file, 'transaction');
-     
+      const errorArray = await this.transactionService.processTransaction(
+        file,
+        "transaction",
+      );
 
       // Prepare data for the Sheet entity
       const sheetData: Partial<Sheet> = {
         uploadedAt: new Date(), // Current date and time
-        uploadedAtTime: format(new Date(), 'hh:mm a'), // Format the time as '10:30 AM'
+        uploadedAtTime: format(new Date(), "hh:mm a"), // Format the time as '10:30 AM'
         fileUrl: file.originalname, // Assuming the file path is stored in 'file.path'
-        type: 'Transaction', // Assuming the file path is stored in 'file.path'
+        type: "Transaction", // Assuming the file path is stored in 'file.path'
       };
 
       // Save the Sheet entry to the database
@@ -241,36 +311,43 @@ export class TransactionController {
       return {
         success: true,
         message: Messages.employee.updateBulkSuccess,
-        errorArray
+        errorArray,
       };
     } catch (error) {
-      this.logger.error(`[EmployeeController] [uploadExcel] Error: ${error.message}`);
+      this.logger.error(
+        `[EmployeeController] [uploadExcel] Error: ${error.message}`,
+      );
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
     }
   }
 
-  @Post('upload-fine')
-  @UseInterceptors(FileInterceptor('file'))
-  async uploadFineExcel(@UploadedFile() file: Express.Multer.File): Promise<response<any>> {
+  @Post("upload-fine")
+  @UseInterceptors(FileInterceptor("file"))
+  async uploadFineExcel(
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<response<any>> {
     try {
-      const fileResponse = await this.uploadService.readExcel(file, 'fine');
+      const fileResponse = await this.uploadService.readExcel(file, "fine");
       // Save transaction to the database
-      if ('fine' in fileResponse) {
+      if ("fine" in fileResponse) {
         // Save transaction to the database
-        const filteredTransaction = fileResponse.fine.filter((item) => item !== undefined); // Assuming you have a createBulk method
+        const filteredTransaction = fileResponse.fine.filter(
+          (item) => item !== undefined,
+        ); // Assuming you have a createBulk method
       } else {
-        throw new Error('Unexpected file response type for transaction.');
+        throw new Error("Unexpected file response type for transaction.");
       }
       return {
         success: true,
-        message: 'Fine Allocated',
+        message: "Fine Allocated",
         data: fileResponse.fine,
-        errorArray: fileResponse.errorArray
+        errorArray: fileResponse.errorArray,
       };
     } catch (error) {
-      this.logger.error(`[EmployeeController] [uploadExcel] Error: ${error.message}`);
+      this.logger.error(
+        `[EmployeeController] [uploadExcel] Error: ${error.message}`,
+      );
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
     }
   }
-
 }
